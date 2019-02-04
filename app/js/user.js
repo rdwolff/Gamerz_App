4cfcd2b5221b8f393dc982f47f68bff5cb0171df
Paul de Wolff <pdwolff@gmail.com>
2019-02-04 22:56:44 +0100

    user invoer ok voor nu

--- app/js/user.js
+++ app/js/user.js
@@ -1,30 +1,41 @@
-var saveUser = undefined //hierheen verwijzen wanneer er user gegevens opgeslagen dienen te worden in local storage
+var savedUser = undefined //hierheen verwijzen wanneer er user gegevens opgeslagen dienen te worden in local storage
 
 
 //user new
 
 
-function saveNewUser() {
-
+function saveUser() {
+    console.log("fct_saveUser");
     let user = {}
     user["userName"] = document.getElementById("userName").value;
     user["userEmail"] = document.getElementById("userEmail").value;
     user["userCountry"] = document.getElementById("userCountry").value;
     user["userCity"] = document.getElementById("userCity").value;
-    saveUser = user;
+    savedUser = user;
+    
     SaveToLocalStorage();
-
-    document.getElementById("frm_userNew").reset("frm_userNew");
-    document.location.href = "Settings.html";
-
 }
+
+function saveNewUser() {
+    console.log("functie saveNewUser")
+    saveUser();
+    document.getElementById("frm_userNew").reset("frm_userNew");
+    document.location.href = "index.html";
+
+};
+
+
+
+
+
 
 
 //functie voor opslaan naar locale storage
 function SaveToLocalStorage() {
-    localStorage.setItem("user", JSON.stringify(saveUser));
+    console.log("fuctie SaveToLocalStorage")
+    localStorage.setItem("user", JSON.stringify(savedUser));
     localStorage.setItem("userSaved", true);
-
+    
 };
 
 //functie voor terughalen van locale storage
@@ -32,28 +43,30 @@ function SaveToLocalStorage() {
 
 //page: user
 
-function getUserInfo() {
-    console.log("test");
-		let user = localStorage.getItem("user")
-		let user1 = JSON.parse(user);
+function grabUserInfo() {
+    console.log("fct_grabuser");
+    let user2 = localStorage.getItem("user")
+    let user1 = JSON.parse(user2);
 
-		document.getElementById("userName").value 		= user1.userName
-		document.getElementById("userEmail").value 		= user1.userEmail
-		document.getElementById("userCountry").value 	= user1.userCountry
-        document.getElementById("userCity").value 		= user1.userCity
-        
-	};
+    document.getElementById("userName").value = user1.userName
+    document.getElementById("userEmail").value = user1.userEmail
+    document.getElementById("userCountry").value = user1.userCountry
+    document.getElementById("userCity").value = user1.userCity
+
+    console.log(user1.userName)
+};
 
 
-    function changeUser() {
-  
-      
-       $('.readonly').prop('readonly', false);
-       document.getElementById("userName").focus(); 
-       document.getElementById("btn_saveUser").style.display = 'block';
-       document.getElementById("btn_changeUser").style.display="none";
-    }
+function changeUser() {
 
-    function saveUser() {
 
-    }
+    $('.readonly').prop('readonly', false);
+    document.getElementById("userName").focus();
+    document.getElementById("btn_saveUser").style.display = 'block';
+    document.getElementById("btn_changeUser").style.display = "none";
+}
+
+function deleteUser() {
+    console.log("functie deleteUser")
+   localStorage.clear();
+}
